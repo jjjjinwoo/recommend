13c5e899bc470ac644e37e77b39c7fa889234cc1
박진우 <somes228@gmail.com>
2024-08-09 18:14:25 +0900

    feat: 모바일 서브메뉴 추가

--- js/basic.js
+++ js/basic.js
@@ -1,11 +1,13 @@
 "use strict";
 
+// 언어 지원
+
 const langMenu = document.querySelector("#lang_list");
 
 let i = 0;
 
 function on() {
-  if (i == 0) {
+  if (i) {
     langMenu.style.display = "flex";
     i++;
   } else {
@@ -14,4 +16,29 @@ function on() {
   }
 
   return;
+}
+
+// 모바일 서브메뉴
+let subMenu = document.querySelector(".sub_menu_m");
+let subBlack = document.querySelector("#sub_black");
+let subList = document.querySelector("#sub_menu_m_list");
+
+function onSubmenuM() {
+  subMenu.style.width = "100%";
+
+  subBlack.style.opacity = "1";
+
+  subList.style.transform = "translateX(0px)";
+}
+
+function offSubmenuM2() {
+  subMenu.style.width = "0";
+}
+
+function offSubmenuM() {
+  subBlack.style.opacity = "0";
+
+  subList.style.transform = "translateX(332px)";
+
+  setTimeout(offSubmenuM2, 500);
 }
